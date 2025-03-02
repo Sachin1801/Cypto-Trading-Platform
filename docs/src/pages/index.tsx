dd16d0a40ad501fa7f6ad3697e9194cb3d991b47
Adamay Mann <meadamann2002@gmail.com>
2025-03-01 20:52:20 -0500

    update: change button text

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            Proceed to documentation - 5min ⏱️
           </Link>
         </div>
       </div>
